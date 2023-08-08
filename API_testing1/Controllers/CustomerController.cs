db5f651332f3690958db865732fd018f2fa19a8a
Gonzalo Borderolle <gborderolle@gmail.com>
2023-08-08 02:44:36 -0300

    Initial commit

---
+++ API_testing1/Controllers/CustomerController.cs
@@ -0,0 +1,43 @@
+﻿using API_testing1.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;  // Para List
+using System.Threading.Tasks;     // Para Task
+using System;                     // Para NotImplementedException
+
+namespace API_testing1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : ControllerBase  // Cambié Controller por ControllerBase
+    {
+        [HttpGet]
+        public async Task<List<CustomerDTO>> GetCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<CustomerDTO> GetCustomer(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]  // Cambiado a HttpDelete
+        public async Task<bool> DeleteCustomer(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<CustomerDTO> CreateCustomer(CreateCustomerDTO customer) // Cambiado el nombre del método
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public async Task<CustomerDTO> UpdateCustomer(CustomerDTO customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ API_testing1/DTOs/CustomerDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace API_testing1.DTOs
+{
+    public class CustomerDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
